f0a8298d1109df1ac35ac9c93bdecba3724e8794
mAAdhaTTah <jamesorodig@gmail.com>
2021-11-07 22:40:58 -0500

    Add link model
    Provides a basic REST API in front of my reading links. Will save via API.

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Cron config that gives you an opportunity
@@ -10,12 +10,4 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
  */
 
-module.exports = {
-  /**
-   * Simple example.
-   * Every monday at 1am.
-   */
-  // '0 1 * * 1': () => {
-  //
-  // }
-};
+module.exports = {};
--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  cron: {
+    enabled: true,
+  },
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '30fe56b61efe97e6d85a7b2d3162b8a8'),
+      secret: env("ADMIN_JWT_SECRET", "30fe56b61efe97e6d85a7b2d3162b8a8"),
     },
   },
 });
