10b1c96dcababbb3fdd33234c4ed3d750be71f18
mAAdhaTTah <jamesorodig@gmail.com>
2021-11-07 20:07:32 -0500

    Add production database configuration

---
+++ config/env/production/database.js
@@ -0,0 +1,25 @@
+const parse = require("pg-connection-string").parse;
+
+module.exports = ({ env }) => {
+  const config = parse(env("DATABASE_URL", ""));
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        options: {},
+      },
+    },
+  };
+};
